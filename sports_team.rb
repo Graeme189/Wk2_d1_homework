86e649fb22aa51c37af4611270fd6ee8390670b5
Graeme Henderson <graeme.189@gmail.com>
2019-07-01 20:06:43 +0100

    finishes sports teams

--- sports_team.rb
+++ sports_team.rb
@@ -1,16 +1,27 @@
 class SportsTeam
 
   attr_reader :team, :players
-  attr_accessor :coach
+  attr_accessor :coach, :points
 
-  def initialize(team, players, coach)
+  def initialize(team, players, coach, points)
     @team = team
     @players = players
     @coach = coach
+    @points = points
   end
 
   def add_new_player(name)
     @players.push(name)
+  end
+
+  def check_player(name)
+    @players.include?(name)
+  end
+
+  def points_property(result)
+    if result == 'won'
+      @points += 3
+    end
   end
 
 end
--- sports_team_spec.rb
+++ sports_team_spec.rb
@@ -6,17 +6,28 @@ require_relative('sports_team.rb')
 class TestSportsTeam < MiniTest::Test
 
   def test_change_coach_name
-    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry')
+    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry', 0)
     united.coach = 'Norman'
     assert_equal('Norman', united.coach())
   end
 
-
-
   def test_add_new_player
-    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry')
+    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry', 0)
     united.add_new_player('Harry')
     assert_equal(4, united.players.length)
   end
+
+  def test_check_player__true
+    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry', 0)
+    united.check_player('Ned')
+    assert_equal(true, united.check_player('Ned'))
+  end
+
+  def test_points_property
+    united = SportsTeam.new('Codeclan Athletic', ['Ted', 'Fred', 'Ned'], 'Barry', 0)
+    united.points_property('won')
+    assert_equal(3, united.points)
+  end
+
 
 end
